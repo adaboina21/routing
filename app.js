374ae85a4aa5850318a3ea14cec1f9a3cae73c47
adaboina21 <adaboina@gmail.com>
2020-10-21 15:02:31 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+brnv = require('bank-routing-number-validator');
+//brnv.ABARoutingNumberIsValid('1234567890'); //false
+//brnv.ABARoutingNumberIsValid('021000021') ;//true
+var app = express();app.get('/', function (req, res) {
+   var id = req.query.id;
+    var isvalidrouting= brnv.ABARoutingNumberIsValid(id); //false
+    if(isvalidrouting){
+        res.send("Routing Number Is Valid");
+    }else{
+        res.send("Routing Number Is Not Valid");
+    }
+
+});app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
